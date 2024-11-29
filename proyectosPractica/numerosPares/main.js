d80f0ac0424ab12bf0c9025900e62f8287aba6f8
fcortesbio <66706019+fcortesbio@users.noreply.github.com>
2024-11-29 22:10:20 +0000

    changes on numerosPares

--- proyectosPractica/numerosPares/main.js
+++ proyectosPractica/numerosPares/main.js
@@ -1,21 +1,23 @@
 /* 
 Tareas: 
+
 1. Escuchar cuando hacemos click
+
 2. Escuchar el input / almacenar en una variable
+
 3. Renderizar el output
+
 */
+
 
 let input = document.getElementById("input");
 let calcButton = document.getElementById("calcButton");
 let text = document.getElementById("text");
 
-console.log(input)
-console.log(calcButton)
-console.log(text)
+calcButton.addEventListener("click", ()=>{
+    console.log("click works as expected!")
+});
 
-// calculate.addEventListener("click", ()=>{
-//     console.log("Button is working!")
-// })
 
 function checkPair(integer){
     return integer % 2 === 0 ? "is pair" : "is not pair";
